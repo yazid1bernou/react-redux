d628edca2aea91ae008268a7588cdd205b196487
unknown <yazid1bernou@gmail.com>
2023-12-12 14:18:18 +0000

    first commit

---
+++ src/components/ContainerCake.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContainerCake = () => {
+    return (
+        <div>
+            <h1> Numbers of Cake : </h1>
+            <button> Buy cake</button>
+        </div>
+    )
+}
+
+export default ContainerCake ;
